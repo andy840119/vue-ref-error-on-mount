8e8caa564ca608eb0623d24ff55f4ab93d284bab
為什麼 <andy840119@gmail.com>
2019-02-25 17:32:06 +0800

    add init file

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <test-component ref="testComponent">
+        <a>:P</a>
+    </test-component>
+</template>
+
+<script>
+
+//component
+Vue.component('test-component', httpVueLoader('components/test_component.vue'));
+
+module.exports = {
+    mounted: function() {
+
+        console.log(this.$refs);
+        console.log(this.$refs.testComponent);
+    }
+}
+</script>
